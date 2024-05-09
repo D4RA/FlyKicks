eb3674b3063a534998cc176e248d98f6b495902c
Keith070 <21334269@studentmail.ul.ie>
2024-05-10 00:16:41 +0100

    home page

--- app/src/main/java/com/example/cs4048project/activity/HomeActivity.java
+++ app/src/main/java/com/example/cs4048project/activity/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.example.cs4048project.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.cs4048project.HeaderFooterHelper;
 import com.example.cs4048project.R;
@@ -14,8 +18,55 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        setTitle("Home");
+/*        setTitle("Home");
 
         HeaderFooterHelper.setupHeaderButtons(this);
+
+ */
+
+        CardView exit = findViewById(R.id.cardLogOut);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+        CardView messaging = findViewById(R.id.cardMessaging);
+        messaging.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, MessagingActivity.class));
+            }
+        });
+
+        CardView shop = findViewById(R.id.cardShopPage);
+
+        shop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, ShopActivity.class));
+            }
+        });
+
+        CardView profile = findViewById(R.id.cardLabTest);
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, CustomProfileActivity.class));
+            }
+        });
+
+        CardView logout = findViewById(R.id.cardLogOut);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, LoginFragment.class));
+            }
+        });
+
     }
 }
